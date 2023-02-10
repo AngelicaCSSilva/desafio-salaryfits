63bfa1154dd815273aec0227936cc706d7d854a4
Angélica Silva <angelica.silva9323@gmail.com>
2023-02-10 14:43:57 -0300

    feat: Criado controller da Home

---
+++ lib/home/home_controller.dart
@@ -0,0 +1,26 @@
+import 'package:get/get.dart';
+import 'package:salaryfits_test/repository/i_posts_interface.dart';
+
+class HomeController extends GetxController with StateMixin {
+  final IPostsRepository _dio;
+
+  HomeController(this._dio);
+
+  @override
+  Future<void> onInit() async {
+    super.onInit();
+    getPosts();
+  }
+
+  Future<void> getPosts() async {
+    change([], status: RxStatus.loading());
+    try {
+      final allPosts = await _dio.getAllPosts();
+      change(allPosts, status: RxStatus.success());
+    } catch (e) {
+      change([],
+          status:
+              RxStatus.error('Erro ao carregar a página. Tente novamente.'));
+    }
+  }
+}
