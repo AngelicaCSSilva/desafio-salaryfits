a47b6fddd3535102f47ce26cffb6f148593884dc
Angélica Silva <angelica.silva9323@gmail.com>
2023-02-10 15:08:17 -0300

    feat: Criado CommentCard

---
+++ lib/details/comments/components/comment_card.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class CommentCard extends StatelessWidget {
+  final int id;
+  final String name;
+  final String body;
+
+  const CommentCard(
+      {super.key, required this.id, required this.name, required this.body});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: Column(
+      children: [
+        ListTile(
+          onTap: () => Get.toNamed('/post/$id'),
+          title: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4.0),
+            child: Text(
+              'Usuário: $name',
+              style:
+                  const TextStyle(color: Colors.purple, fontWeight: FontWeight.w500),
+            ),
+          ),
+          subtitle: Text('"$body"',
+              style: const TextStyle(fontStyle: FontStyle.italic)),
+        ),
+      ],
+    ));
+  }
+}
